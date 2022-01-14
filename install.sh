18de0bc5f13c4e68aa33d2eb02001b9285a16d68
Brendan Baldwin <brendan@usergenic.com>
2022-01-14 14:53:58 -0700

    Updated install.sh so that it installs pip and uses venv.

--- install.sh
+++ install.sh
@@ -3,9 +3,16 @@
 source ./install/functions.sh
 # TODO(usergenic): consider virtualenv and --user install of dependencies
 # https://gist.github.com/saurabhshri/46e4069164b87a708b39d947e4527298#gistcomment-2271969
-pip3 install virtualenv --user
-virtualenv beholder2
-pip3 install -r ./install/pip-package-list.txt --user
+
+download_file \
+  'https://bootstrap.pypa.io/get-pip.py' \
+  get-pip.py
+
+python3 -m venv .
+source bin/activate
+python3 get-pip.py
+python3 -m pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+python3 -m pip install -r ./install/pip-package-list.txt
 
 mkdir -p ext/data
 mkdir -p ext/lib
