23b5f6b9856a6cab77ae695e8849d17e895546ee
Brendan Baldwin <brendan@usergenic.com>
2022-02-03 00:25:05 -0700

    image-compare.sh script fix when images identical.

--- image-compare.sh
+++ image-compare.sh
@@ -1,5 +1,5 @@
 result=$(compare $1 $2 -metric RMSE /dev/null 2>&1|grep -o "([0-9.]\+)"|grep -o "[0-9]\\.[0-9][0-9]"|sed "s/\.//"|sed "s/^0*//")
-if [ $result == "" ]; then
+if [ "$result" == "" ]; then
   echo "0"
 else
   echo $result
